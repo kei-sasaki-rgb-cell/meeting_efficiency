47825f59014bd237ea637cab5b46455dcae7d7f6
kei-sasaki-rgb-cell <11eftx2tree@gmail.com>
2021-05-15 14:27:34 +0900

    アクティブハッシュ修正

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,8 +1,8 @@
 class Information < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '1.多すぎた、もしくは少なすぎた' },
-    { id: 3, name: '2.多い、もしくは少ない' },
+    { id: 2, name: '1.多すぎたもしくは少なすぎた' },
+    { id: 3, name: '2.多かったもしくは少ない' },
     { id: 4, name: '3.普通' },
     { id: 5, name: '4.良かった' },
     { id: 6, name: '5.丁度良かった' }
